1e7b3203e934e6eb146881213c480c02417749e8
Richard Hitier <richard.hitier@co-libri.org>
2015-10-31 21:31:33 +0100

    simple sh client

---
+++ simpleClient.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+wgetOpt=-O-
+myKey=44ce07402c4c21ca26071733f9c80077
+mySlug=hva-evts
+
+# get my agenda uid
+wget $wgetOpt https://api.openagenda.com/v1/agendas/uid/$mySlug?key=$myKey 2>/dev/null
+
+
+
+
+# vim: tw=0
