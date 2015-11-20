e68c94318f846bbbdacc0896369723a51f6d4521
Richard Hitier <richard.hitier@co-libri.org>
2015-11-20 16:29:21 +0100

    add agendaUid variable

--- simpleClient.sh
+++ simpleClient.sh
@@ -3,14 +3,17 @@
 wgetOpt=-O-
 myKey=44ce07402c4c21ca26071733f9c80077
 agendaSlug=hva-evts
+agendaUid=18389556
 
 # get my agenda uid
+echo 'my agenda uid resquest'
 wget $wgetOpt https://api.openagenda.com/v1/agendas/uid/$agendaSlug?key=$myKey 2>/dev/null
 
 echo ' ' 
 
 # get my agenda all evts
-wget $wgetOpt https://api.openagenda.com/v1/agendas/18389556?key=$myKey 2>/dev/null
+echo 'my agenda events resquest'
+wget $wgetOpt https://api.openagenda.com/v1/agendas/$agendaUid?key=$myKey 2>/dev/null
 
 
 
