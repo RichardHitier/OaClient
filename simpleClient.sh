cfd0016bcee60d5c97c9ab2542f86322b4f66880
Richard Hitier <richard.hitier@co-libri.org>
2015-11-20 16:34:02 +0100

    get my agenda's all events

--- simpleClient.sh
+++ simpleClient.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
 wgetOpt=-O-
+wgetBin="wget $wgetOpt "
 myKey=44ce07402c4c21ca26071733f9c80077
 agendaSlug=hva-evts
 agendaUid=18389556
 
 # get my agenda uid
-echo 'my agenda uid resquest'
-wget $wgetOpt https://api.openagenda.com/v1/agendas/uid/$agendaSlug?key=$myKey 2>/dev/null
-
+echo "get agenda uid resquest"
+$wgetBin https://api.openagenda.com/v1/agendas/uid/$agendaSlug?key=$myKey 2>/dev/null
 echo ' ' 
 
-# get my agenda all evts
-echo 'my agenda events resquest'
-wget $wgetOpt https://api.openagenda.com/v1/agendas/$agendaUid?key=$myKey 2>/dev/null
+# 
+echo "get my agenda's description"
+$wgetBin https://api.openagenda.com/v1/agendas/$agendaUid?key=$myKey 2>/dev/null
+echo ' ' 
+
+# 
+echo "get my agenda events "
+$wgetBin https://api.openagenda.com/v1/agendas/$agendaUid/events?key=$myKey 2>/dev/null
+
 
 
 
